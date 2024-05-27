ae6a41cf142788c6b41477fc6ecf2fe4a5204089
João Paulo de Oliveira <jpos.usp@gmail.com>
2024-05-27 16:05:55 -0300

    add cross validation and early stopping training methods

--- src/main/kotlin/mlp/MLP.kt
+++ src/main/kotlin/mlp/MLP.kt
@@ -43,6 +43,64 @@ class MLP(
         saveWeights()
     }
 
+    fun crossValidate(
+        inputs: List<List<Double>>,
+        targets: List<List<Double>>,
+        k: Int,
+        epochs: Int,
+        learningRate: Double
+    ): Double {
+        val foldSize = inputs.size / k
+        var totalMSE = 0.0
+
+        for (i in 0 until k) {
+            val validationStart = i * foldSize
+            val validationEnd = validationStart + foldSize
+
+            val trainingInputs = inputs.take(validationStart) + inputs.drop(validationEnd)
+            val trainingTargets = targets.take(validationStart) + targets.drop(validationEnd)
+
+            val validationInputs = inputs.subList(validationStart, validationEnd)
+            val validationTargets = targets.subList(validationStart, validationEnd)
+
+            val mlp = createMLP() // Certifique-se de ter um método para criar uma nova instância de MLP
+            mlp.train(trainingInputs, trainingTargets, epochs, learningRate)
+            totalMSE += mlp.test(validationInputs, validationTargets)
+        }
+
+        return totalMSE / k
+    }
+
+    fun trainWithEarlyStopping(
+        inputs: List<List<Double>>,
+        targets: List<List<Double>>,
+        validationInputs: List<List<Double>>,
+        validationTargets: List<List<Double>>,
+        epochs: Int,
+        learningRate: Double,
+        patience: Int
+    ) {
+        var bestMSE = Double.MAX_VALUE
+        var epochsWithoutImprovement = 0
+
+        for (epoch in 1..epochs) {
+            train(inputs, targets, 1, learningRate)
+            val mse = test(validationInputs, validationTargets)
+
+            if (mse < bestMSE) {
+                bestMSE = mse
+                epochsWithoutImprovement = 0
+            } else {
+                epochsWithoutImprovement++
+                if (epochsWithoutImprovement >= patience) {
+                    println("Parada antecipada na epoca $epoch")
+                    break
+                }
+            }
+        }
+    }
+
+
     private fun backpropagate(error: List<Double>, learningRate: Double) {
         var currentError = error
         for (layer in layers.reversed()) {
